830e2300cd5b4389d355522fef4fb150c264cd29
UmbertoDV <biccoumby2002@hotmail.com>
2023-03-30 19:28:51 +0200

    created migration for `trains`

---
+++ database/migrations/2023_03_30_164840_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->string('orario_partenza');
+            $table->string('orario_arrivo');
+            $table->integer('codice_treno')->unsigned();
+            $table->integer('n_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
